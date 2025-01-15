3d357e3f608a882f1646c36bba518cd664dcf561
Nizamov17 <13_artur.nizamov_13@mail.ru>
2025-01-15 12:49:08 +0300

    two commit

---
+++ AppNizamov/AppNizamov/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppNizamov
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppNizamov/AppNizamov/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppNizamov
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
